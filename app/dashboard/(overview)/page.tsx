abcdbc061d64017dbf4610c067bf27865d4b74a9
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-10-15 18:12:34 +0530

    next auth

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -8,16 +7,9 @@ import {
 	RevenueChartSkeleton,
 	CardsSkeleton,
 } from "@/app/ui/skeletons";
-import { fetchCardData } from "@/app/lib/data";
 import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
-	const {
-		numberOfCustomers,
-		numberOfInvoices,
-		totalPaidInvoices,
-		totalPendingInvoices,
-	} = await fetchCardData();
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
@@ -18,15 +18,17 @@ const config: Config = {
 					600: "#2F6FEB",
 				},
 			},
-		},
-		keyframes: {
-			shimmer: {
-				"100%": {
-					transform: "translateX(100%)",
+			keyframes: {
+				shimmer: {
+					"100%": { transform: "translateX(100%)" },
 				},
+			},
+			animation: {
+				shimmer: "shimmer 1.5s infinite",
 			},
 		},
 	},
 	plugins: [require("@tailwindcss/forms")],
 };
+
 export default config;
